7db72c9ab8414a18e9e3303e2b5e760896e0f043
Amna Hamid Ali <amnahamid346090@gmail.com>
2026-03-18 07:08:12 +0500

    added CORS, morgan logging, rate limiting, cookie parsing, and HTTP proxying to the API Gateway, enabling it to route requests to the Auth Service. The Auth Service has been configured with CORS support

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,13 +1,47 @@
 import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import rateLimit from 'express-rate-limit';
+import proxy from 'express-http-proxy';
+import cookieParser from 'cookie-parser';
 
 const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const port = process.env.PORT ? Number(process.env.PORT) : 8080;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
+app.use(cors(
+  {
+    origin: 'http://localhost:3000',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  }
+));
+
+app.use(morgan('dev'));
+
+app.use(cookieParser());
+
+app.set('trust proxy', 1);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: '100mb' }));
+
+app.use(rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: (req:any) => (req.user ? 1000 : 100),
+  message: 'Too many requests, please try again later.',
+  standardHeaders: true,
+  legacyHeaders: false,
+  keyGenerator: (req: any) => req.user?.id || req.ip,
+}));
+
+app.get('/gateway-health', (req, res) => {
+  res.send({ message: 'Gateway is healthy' });
 });
+
+
+app.use('/', proxy('http://localhost:6001'));
 
 app.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,14 +1,24 @@
 import express from 'express';
-
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+import cors from 'cors';
 
 const app = express();
+app.use(cors(
+  {
+    origin: 'http://localhost:3000',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  }
+));
 
-app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
+const port = process.env.PORT ? Number(process.env.PORT) : 6001;
+const server = app.listen(port, () => {
+  console.log(`[ ready ] http://localhost:${port}`);
 });
 
-app.listen(port, host, () => {
-  console.log(`[ ready ] http://${host}:${port}`);
+app.get('/', (req, res) => {
+  res.send({ message: 'Auth Service is healthy' });
+});
+
+server.on('error', (error) => {
+  console.error(error);
 });
